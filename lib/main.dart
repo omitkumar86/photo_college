7e1f10ac5dc70c23200f6d534ef977f2b84d6736
omitkumar86 <omitkumar86@gmail.com>
2023-07-13 00:20:18 +0600

    Updated -- main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:get/get_navigation/src/routes/get_route.dart';
+import 'package:get/get_navigation/src/routes/transitions_type.dart';
 import 'package:photo_college/utils/app_constants.dart';
 import 'package:photo_college/view/screen/home/home_screen.dart';
 import 'package:photo_college/view/screen/on_boarding/on_boarding_screen_01.dart';
@@ -10,7 +11,7 @@ import 'package:photo_college/view/screen/on_boarding/on_boarding_screen_03.dart
 import 'package:photo_college/view/screen/splash/splash_screen.dart';
 import 'di_container.dart' as di;
 
-void main() async{
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await di.init();
   runApp(const MyApp());
@@ -36,11 +37,35 @@ class MyApp extends StatelessWidget {
             ),
             initialRoute: SplashScreen.routeName,
             getPages: [
-              GetPage(name: SplashScreen.routeName, page: () => SplashScreen()),
-              GetPage(name: OnBoardingScreen01.routeName, page: () => OnBoardingScreen01()),
-              GetPage(name: OnBoardingScreen02.routeName, page: () => OnBoardingScreen02()),
-              GetPage(name: OnBoardingScreen03.routeName, page: () => OnBoardingScreen03()),
-              GetPage(name: HomeScreen.routeName, page: () => HomeScreen()),
+              GetPage(
+                name: SplashScreen.routeName,
+                page: () => SplashScreen(),
+                transition: Transition.fadeIn,
+              ),
+              GetPage(
+                name: OnBoardingScreen01.routeName,
+                page: () => OnBoardingScreen01(),
+                transition: Transition.leftToRight,
+                transitionDuration: Duration(milliseconds: 500),
+              ),
+              GetPage(
+                name: OnBoardingScreen02.routeName,
+                page: () => OnBoardingScreen02(),
+                transition: Transition.leftToRight,
+                transitionDuration: Duration(milliseconds: 500),
+              ),
+              GetPage(
+                name: OnBoardingScreen03.routeName,
+                page: () => OnBoardingScreen03(),
+                transition: Transition.leftToRight,
+                transitionDuration: Duration(milliseconds: 500),
+              ),
+              GetPage(
+                name: HomeScreen.routeName,
+                page: () => HomeScreen(),
+                transition: Transition.leftToRight,
+                transitionDuration: Duration(milliseconds: 500),
+              ),
             ],
           );
         });
